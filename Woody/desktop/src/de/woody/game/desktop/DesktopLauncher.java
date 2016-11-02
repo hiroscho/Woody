3f71e6ad6d25bb613322fe371bc5e4226aa3d640
Thomas Lehner <thomas__lehner@web.de>
2016-11-02 11:25:33 +0100

    Init

---
+++ Woody/desktop/src/de/woody/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.woody.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.woody.game.WoodyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WoodyGame(), config);
+	}
+}

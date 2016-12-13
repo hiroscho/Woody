89e2782f1b55b35e99a72e5655b614d7e0077168
Thomas Lehner <thomas__lehner@web.de>
2016-12-13 18:46:13 +0100

    Only sets up the stage and provides methods to create buttons and loop through all available buttons of the stage.

--- Woody/core/src/de/woody/game/Buttons.java
+++ Woody/core/src/de/woody/game/Buttons.java
@@ -4,144 +4,86 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+public class Buttons extends WoodyGame {
+	private Stage stage;
+	private TextureRegion buttonTextureRegion;
+	private TextureRegionDrawable buttonTexRegionDrawable;
+	private ImageButton button;
+	private Viewport viewport;
+	private boolean jumpPressed;
+	private boolean attackPressed;
+	private boolean leftPressed;
+	private boolean rightPressed;
+	private Array<Button> allButtons = new Array<Button>();
 
-public class Buttons extends WoodyGame
-{
-   Viewport viewport;
-   Stage stage;
-   static boolean jumpPressed;
-   static boolean attackPressed;
-   static boolean leftPressed;
-static boolean rightPressed;
-   OrthographicCamera cam;
-   int sizeVariable = 90;
-   
-   public Buttons(){
-       cam = new OrthographicCamera();
-       viewport = new FitViewport(1280, 768, cam);
-       stage = new Stage(viewport);
-       Gdx.input.setInputProcessor(stage);
-       
-       Table table = new Table();
-       table.left().bottom();
-       
-       Image jumpImg = new Image(new Texture("textures/ButtonJump.png"));
-       jumpImg.setSize(sizeVariable, sizeVariable);
-       jumpImg.addListener(new InputListener(){
-           @Override
-           public boolean touchDown(InputEvent event, float x, float y, int pointer, int button){
-               jumpPressed = true;
-               return true;
-           }
-           
-           @Override
-           public void touchUp(InputEvent event, float x, float y, int pointer, int button){
-               attackPressed = false;
-           }
-       });
-       
-       Image rightImg = new Image(new Texture("textures/ButtonRight.png"));
-       rightImg.setSize(sizeVariable, sizeVariable);
-       rightImg.addListener(new InputListener(){
-           @Override
-           public boolean touchDown(InputEvent event, float x, float y, int pointer, int button){
-               rightPressed = true;
-               return true;
-           }
-           
-           @Override
-           public void touchUp(InputEvent event, float x, float y, int pointer, int button){
-               rightPressed = false;
+	public Buttons() {
+		// create a new viewport for the ui
+		viewport = new StretchViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), new OrthographicCamera());
+		
+		// set up a stage for the ui
+		stage = new Stage(viewport);
 
-           }
-       });
-       
-       Image leftImg = new Image(new Texture("textures/ButtonLeft.png"));
-       leftImg.setSize(sizeVariable, sizeVariable);
-       leftImg.addListener(new InputListener(){
-           @Override
-           public boolean touchDown(InputEvent event, float x, float y, int pointer, int button){
-               leftPressed = true;
-               return true;
-           }
-           
-           @Override
-           public void touchUp(InputEvent event, float x, float y, int pointer, int button){
-               leftPressed = false;
-           }
-       });
-       
-       Image attackImg = new Image(new Texture("textures/ButtonFight.png"));
-       attackImg.setSize(sizeVariable, sizeVariable);
-       attackImg.addListener(new InputListener(){
-           @Override
-           public boolean touchDown(InputEvent event, float x, float y, int pointer, int button){
-               attackPressed = true;
-               return true;
-           }
-           
-           @Override
-           public void touchUp(InputEvent event, float x, float y, int pointer, int button){
-               attackPressed = false;
-           }
-       });
-       
-//       table.add(jumpImg).size(jumpImg.getWidth(), jumpImg.getHeight());
-//       table.row().pad(5,5,5,5);
-//       table.add(leftImg).size(leftImg.getWidth(), leftImg.getHeight());
-//       table.add(rightImg).size(rightImg.getWidth(), rightImg.getHeight());
-//       table.row().padBottom(5);
-//       table.add(attackImg).size(attackImg.getWidth(), attackImg.getHeight());
+	}
+	
+	public Button addButton(Texture tex, String name) {
+		// making an annoying drawable...
+		buttonTextureRegion = new TextureRegion(tex);
+		buttonTexRegionDrawable = new TextureRegionDrawable(buttonTextureRegion);
+		
+		// set the button up
+		button = new ImageButton(buttonTexRegionDrawable); 
+		button.setName(name);
+		
+		//add the button to the list of all buttons
+		allButtons.add(button);
+		
+		// add the button to the stage to perform rendering and take input
+		stage.addActor(button);
+		
+		return button;
+	}
 
-       table.add();
-       table.add(jumpImg).size(jumpImg.getWidth(), jumpImg.getHeight());
-       table.add();
-       
-       table.row().pad(5,5,5,5);
-       
-       table.add(leftImg).size(leftImg.getWidth(), leftImg.getHeight());
-       table.add();
-       table.add(rightImg).size(rightImg.getWidth(), rightImg.getHeight());
-       
-       table.row().padBottom(5);
-       
-       table.add();
-       table.add(attackImg).size(attackImg.getWidth(), attackImg.getHeight());
-       table.add();
-       
-       stage.addActor(table);
-   }
-       
-   public void draw(){
-       stage.draw();
-   }
-   
-   public boolean isjumpPressed(){
-       return jumpPressed;
-   }
-   public boolean isLeftPressed(){
-       return leftPressed;
-   }
-   public boolean isRightPressed(){
-       return rightPressed;
-   }
-   public boolean isattackPressed(){
-       return attackPressed;
-   }
-   
-   public void resize(int width, int height){
-       viewport.update(width, height);
-   }
-} 
+	public Stage getStage() {
+		return stage;
+	}
+
+	public void draw() {
+		stage.draw();
+	}
+
+	public boolean isjumpPressed() {
+		return jumpPressed;
+	}
+
+	public boolean isLeftPressed() {
+		return leftPressed;
+	}
+
+	public boolean isRightPressed() {
+		return rightPressed;
+	}
+
+	public boolean isattackPressed() {
+		return attackPressed;
+	}
+
+	public void resize(int width, int height) {
+		viewport.update(width, height);
+	}
+
+	public Button checkAllButtons() {
+		for (Button but : allButtons) {
+			if (but.isPressed())
+				return but;
+		}
+		return null;
+	}
+}

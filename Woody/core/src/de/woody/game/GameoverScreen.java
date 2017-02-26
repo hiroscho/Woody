c292dd5202f00b8070923c4ec28b26dd6b87bd98
Thomas Lehner <thomas__lehner@web.de>
2017-02-26 13:24:53 +0100

    Fixed the batch.

--- Woody/core/src/de/woody/game/GameoverScreen.java
+++ Woody/core/src/de/woody/game/GameoverScreen.java
@@ -12,80 +12,78 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-public class GameoverScreen extends WoodyGame implements Screen{
+public class GameoverScreen extends WoodyGame implements Screen {
 
 	private Viewport viewport;
 	private Stage gameoverstate;
 	private Game gameover;
-	
-	
+	private Batch batch;
+
 	private static final int BANNER_WIDTH = 350;
 	private static final int BANNER_HEIGHT = 100;
 	Texture gameOverBanner = new Texture("textures/Gameoverscreen.png");
-	
-	public GameoverScreen(GameoverScreen gameoverScreen) {
+
+	public GameoverScreen(WoodyGame game) {
 		this.gameover = game;
+		batch = new SpriteBatch();
 	}
 
 	@Override
-	public void create(){
+	public void create() {
 		this.setScreen(new GameoverScreen(this));
 	}
 
-	
-	
-//	public GameoverScreen(Game game){
-//		this.game = game;
-//		viewport = new FitViewport(Player.WIDTH, Player.HEIGHT, new OrthographicCamera());
-//		gameover = new Stage(viewport, new SpriteBatch());
-//	}
-	
-	
+	// public GameoverScreen(Game game){
+	// this.game = game;
+	// viewport = new FitViewport(Player.WIDTH, Player.HEIGHT, new
+	// OrthographicCamera());
+	// gameover = new Stage(viewport, new SpriteBatch());
+	// }
+
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		Batch batch = (Batch) game;
 		batch.begin();
-		batch.draw(gameOverBanner, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch.draw(gameOverBanner, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.end();
-		
+
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
